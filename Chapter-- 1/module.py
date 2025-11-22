5665578627a6fcb20c73c317fa93297c4f6d586e
Yuvrraj Arora <yuvrrajarora@gmail.com>
2025-11-22 19:04:16 +0530

    Update and rename n to module.py

---
+++ Chapter-- 1/module.py
@@ -0,0 +1,12 @@
+import pyjokes
+
+# print("Jai mata di")
+# this print a random jokes 
+joke = pyjokes.get_joke()
+print (joke)
+"""so thanks this was
+my program
+thank you to read"""
+
+# comment 
+# not comment 
