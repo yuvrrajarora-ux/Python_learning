fedb0ea1f07c1cd82589a5d6abb7b23889794b8b
Yuvrraj Arora <yuvrrajarora@gmail.com>
2025-11-28 19:18:47 +0530

    Add files via upload

---
+++ Chapter-- 2/04_operators.py
@@ -0,0 +1,39 @@
+#arithematic operators
+a = 1
+b = 35
+c = a + b
+print(c)
+
+
+#Assignment Operators
+
+a = 48-2
+print (a)
+b = 5
+#b += 7 #Increment the value of b by 7 and then assign it to b
+b *= 8 # decrement the value of b by 8 and then assign it  to b
+print (b)
+
+# Comparison Operators
+
+d = 5==5
+
+print (d)
+
+e = True and False
+print (e)
+
+# Truth table of 'or'
+print ("True or false is", True or False)
+print ("True or True is", True or True)
+print ("False or True is", True or False)
+print ("False or False is", False or False)
+
+# Truth table of 'and'
+print ("True and False is", True and False)
+print ("True and true is", True and True)
+print ("False and True is", False and True)
+print ("False and False is", False and False)
+
+print(not(False))
+print (not(True))
