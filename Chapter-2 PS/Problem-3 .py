b51b79a0962c5953102febf27ea8718bb6c55ad6
Yuvrraj Arora <yuvrrajarora@gmail.com>
2026-09-13 22:59:29 +0530

    Python Input and Type Conversion
    This program demonstrates that values received using the input() function are stored as strings by default. It then converts the input value into a float using type casting and displays the updated data type.

--- Chapter-2 PS/Problem-3 .py
+++ Chapter-2 PS/Problem-3 .py
@@ -1,6 +1,12 @@
-a =(input ("Enter the value of a:"))
-print(type(a))
+# Check the data type of a variable assigned using the input() function
 
-a = float
+a = input("Enter the value of a: ")
 
-print (a)
+print("Value entered:", a)
+print("Data type:", type(a))
+
+# Convert the input value to a float
+a = float(a)
+
+print("Converted value:", a)
+print("New data type:", type(a))
